7b9dddef9c21aea445e5cd721955bf6c69398291
madalin.antonoiu <madalin.antonoiu@crunch.co.uk>
2023-10-30 22:44:46 +0000

    Using the new ListCard component

--- components/chainHoldings.js
+++ components/chainHoldings.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react'
 import formatToDollar from '@/utils/helpers'
 import { chainMapping } from '@/utils/tokenMapping'
-import styles from './chainHolding.module.scss'
-import Card from '@/components/library/Card'
+import ListCard from '@/components/library/ListCard'
 
 const ChainHoldings = () => {
   const [data, setData] = useState(null)
@@ -18,29 +17,7 @@ const ChainHoldings = () => {
     return <div>Loading...</div>
   }
 
-  return (
-    <Card>
-      <div className='flex flex-col mb-8 w-2/3 mx-auto pb-8 font-inter text-white w-full'>
-        <div className={styles.title}>
-          <div className='flex-1 font-bold text-xl font-extrabold'>Chain</div>
-          <div className='flex-1 font-bold text-xl font-extrabold'>Total Holdings</div>
-        </div>
-        <div className={styles.list}>
-          {Object.entries(data)
-            .sort((a, b) => b[1] - a[1])
-            .map(([key, value], index) => (
-              <div key={index} className={`flex py-2 px-4 mb-1 ${index % 2 === 0 ? 'bg-gray-200' : 'bg-gray-600'}`}>
-                <div className={styles.chain}>
-                  <img src={chainMapping[key]} alt='' style={{ width: '22px', height: '22px' }} className='mr-2 ' />
-                  {key}
-                </div>
-                <div className={styles.holdings}>{formatToDollar(value)}</div>
-              </div>
-            ))}
-        </div>
-      </div>
-    </Card>
-  )
+  return <ListCard title1='Chain' title2='Total Holdings' data={data} mapping={chainMapping} />
 }
 
 export default ChainHoldings
