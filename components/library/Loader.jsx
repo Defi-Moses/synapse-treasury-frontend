8fecf481ad4bc3a8add22200a7142fb683956841
madalin.antonoiu <madalin.antonoiu@crunch.co.uk>
2023-10-31 02:09:41 +0000

    className instead of class

--- components/library/Loader.jsx
+++ components/library/Loader.jsx
@@ -2,7 +2,7 @@ import styles from './Loader.module.scss'
 
 export default function Loader() {
   return (
-    <div class={styles.ellipsis}>
+    <div className={styles.ellipsis}>
       <div></div>
       <div></div>
       <div></div>
