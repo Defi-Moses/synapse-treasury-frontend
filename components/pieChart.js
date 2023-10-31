994faa552d045ad32218fdb3af7920ee8b2f04e5
manzuni <20832842+manzuni@users.noreply.github.com>
2023-10-31 15:27:44 +0000

    PieChart - Responsive Mobile (#1)
    * Added  Prettier config
    
    * Added SASS - Tailwind & D3 can't cover all edge cases
    
    * Added @ alias for everything, now just the app folder
    
    * Added .prettierc config
    
    * Single quotes for JSX too
    
    * Pie Chart - Mobile Responsiveness
    
    * Removed uneccessary comment
    
    * Moved Last Updated message at the top - makes sense since it is about the entire page data
    
    * Added page.module.scss
    
    ---------
    
    Co-authored-by: madalin.antonoiu <madalin.antonoiu@crunch.co.uk>

--- components/pieChart.js
+++ components/pieChart.js
@@ -1,89 +1,85 @@
 // treasury-frontend/pages/components/PieChart.js
 import * as d3 from 'd3'
 import { useEffect, useRef, useState } from 'react'
-import formatToDollar from '../utils/helpers'
+import formatToDollar from '@/utils/helpers'
+import styles from './pieChart.module.scss'
 
 const PieChart = () => {
-  const ref = useRef();
-  const [data, setData] = useState(null);
-  const [legendData, setLegendData] = useState([]);
-  const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+  const ref = useRef()
+  const [data, setData] = useState(null)
+  const [legendData, setLegendData] = useState([])
+  const colorScale = d3.scaleOrdinal(d3.schemeCategory10)
 
   useEffect(() => {
     fetch('/api/data?file=sums.csv&type=summary')
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setData(data)
         setLegendData(Object.keys(data))
       })
-      .catch(error => console.error('Error:', error));
-  }, []);
+      .catch((error) => console.error('Error:', error))
+  }, [])
 
   useEffect(() => {
     if (data && Object.keys(data).length > 0) {
-      const svg = d3.select(ref.current);
-      const width = svg.node().getBoundingClientRect().width;
-      const height = svg.node().getBoundingClientRect().height;
-      const radius = Math.min(width, height) * 0.8 / 2;
+      const svg = d3.select(ref.current)
+      const width = svg.node().getBoundingClientRect().width
+      const height = svg.node().getBoundingClientRect().height
+      const radius = (Math.min(width, height) * 0.8) / 2
 
       // const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
-      const tooltip = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+      const tooltip = d3.select('body').append('div').attr('class', 'tooltip').style('opacity', 0)
 
-      svg.attr('viewBox', [-width / 2, -height / 2, width, height]);
+      svg.attr('viewBox', [-width / 2, -height / 2, width, height])
 
-      const pie = d3.pie().value(d => d[1])(Object.entries(data));
+      const pie = d3.pie().value((d) => d[1])(Object.entries(data))
 
-      const arc = d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius)
+      const arc = d3.arc().innerRadius(0).outerRadius(radius)
 
       svg
-      .selectAll('path')
-      .data(pie)
-      .join('path')
-      .attr('d', arc)
-      .attr('fill', (d,i) => colorScale(i))
-      .on('mouseover', (event, d) => {
-        tooltip.transition()
-          .duration(200)
-          .style("opacity", .9);
-        tooltip.html(`${d.data[0]}: ${formatToDollar(d.data[1])}`)
-          .style("left", (event.pageX) + "px")
-          .style("top", (event.pageY - 28) + "px");
-      })
-      .on('mouseout', (d) => {
-        tooltip.transition()
-          .duration(500)
-          .style("opacity", 0);
-      });
+        .selectAll('path')
+        .data(pie)
+        .join('path')
+        .attr('d', arc)
+        .attr('fill', (d, i) => colorScale(i))
+        .on('mouseover', (event, d) => {
+          tooltip.transition().duration(200).style('opacity', 0.9)
+          tooltip
+            .html(`${d.data[0]}: ${formatToDollar(d.data[1])}`)
+            .style('left', event.pageX + 'px')
+            .style('top', event.pageY - 28 + 'px')
+        })
+        .on('mouseout', (d) => {
+          tooltip.transition().duration(500).style('opacity', 0)
+        })
     }
-  }, [data]);
-
-
+  }, [data])
 
   if (!data) return <p>Loading...</p>
   return (
-    <div className="flex flex-col items-start border rounded-lg py-8 px-4 ml-4">
-
-    <div className="flex flex-row justify-center items-center">
-      <svg className="w-1/4 py-4 ml-8" style={{ height: `${legendData.length * 28}px` }}>
-        {legendData.map((key, i) => (
-          <g key={key} transform={`translate(0,${i * 20})`}>
-            <rect x={0} width={18} height={18} fill={colorScale(i)} />
-            <text x={24} y={9} dy=".35em" fill="white">{key}</text>
-          </g>
-        ))}
-      </svg>
-      <svg ref={ref} className=""/>
+    <div className={styles.container}>
+      <div className='flex justify-center font-bold text-white w-full'>
+        {`Total Treasury: ${formatToDollar(Object.values(data).reduce((a, b) => a + b, 0))}`}
+      </div>
+      <div className={`flex flex-col md:flex-row justify-center items-center px-4`}>
+        <svg ref={ref} className={styles.chart} />
+        <div className={styles.legend}>
+          {legendData.map((key, i) => (
+            <div key={key} className={styles.legendItem}>
+              <div
+                style={{
+                  width: '18px',
+                  height: '18px',
+                  backgroundColor: colorScale(i),
+                }}
+              ></div>
+              <span className='ml-2 mr-2 text-white'>{key}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-
-    <div className="p-8 font-bold text-white">
-      {`Total Treasury: ${formatToDollar(Object.values(data).reduce((a, b) => a + b, 0))}`}
-    </div>
-    </div>
-  );
+  )
 }
 
 export default PieChart
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,42 @@
-"use client"
+'use client'
 import Image from 'next/image'
-import React from 'react';
+import React from 'react'
 import ChainHoldings from '../../components/chainHoldings.js'
 import TokenList from '../../components/tokenList.js'
 import MonthList from '../../components/pastHoldings.js'
 import PieChart from '../../components/pieChart.js'
-import dynamic from 'next/dynamic';
-import { ApolloProvider } from '@apollo/client';
-import client from '../../utils/apollo-client';
+import dynamic from 'next/dynamic'
+import { ApolloProvider } from '@apollo/client'
+import client from '../../utils/apollo-client'
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <div className='bg-black'>
-    <ApolloProvider client={client}>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold font-inter text-white">Synapse Treasury Holdings</h1>
-      <div className= "w-2/3 h-full py-4">
-        <h3 className="font-bold font-inter text-white">Last Updated 10-22-2023</h3>
-        <PieChart/> 
-      </div>
-      <div className="flex justify-between w-2/3 mx-auto">
-        <div className="flex-1 border border-white p-4 rounded-lg mr-2">
-          <ChainHoldings/>
-          
-        </div>
-        <div className="flex-1 border border-white rounded-lg p-4 ml-2">
-          <TokenList />
-        </div>
-      </div>
-      <h3 className="font-bold font-inter py-4 text-white">Historical Data</h3>
-      <div className= "w-2/3 h-full p-8 border rounded-lg">
-      
-        <MonthList/>
-      </div>
+      <ApolloProvider client={client}>
+        <main className='flex min-h-screen flex-col items-center justify-between p-6 md:p-24'>
+          <h1 className='text-xl md:text-4xl font-bold font-inter text-white'>Synapse Treasury Holdings</h1>
+          <div className='flex items-center justify-center py-4 w-full'>
+            <span className={styles.badge}>Last Updated 10-22-2023</span>
+          </div>
+          <div className='w-full md:w-2/3 h-full py-8'>
+            <PieChart />
+          </div>
+          <div className='flex flex-col sm:flex-row justify-between w-full sm:w-3/3 '>
+            <div className='flex-1 border border-white p-4 rounded-lg'>
+              <ChainHoldings />
+            </div>
+            <div className='flex-1 border border-white rounded-lg p-4 ml-2'>
+              <TokenList />
+            </div>
+          </div>
+          <h3 className='font-bold font-inter py-4 text-white'>Historical Data</h3>
+          <div className='w-2/3 h-full p-8 border rounded-lg'>
+            <MonthList />
+          </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-      
-        {/* <Image
+          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+            {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
@@ -44,17 +44,20 @@ export default function Home() {
           height={37}
           priority
         /> */}
-      </div>
-
-
-    </main>
-    <div className="w-1/8 flex justify-center items-center bg-black ">
-      <a href="https://github.com/Defi-Moses/synapse-treasury/blob/main/README.md#methodology" target="_blank" rel="noopener noreferrer" className="text-white">
-        Methodology can be found 
-        <span className="text-blue-500"> here</span>
-      </a>
-    </div>
-    </ApolloProvider>
+          </div>
+        </main>
+        <div className='w-1/8 flex justify-center items-center bg-black '>
+          <a
+            href='https://github.com/Defi-Moses/synapse-treasury/blob/main/README.md#methodology'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-white'
+          >
+            Methodology can be found
+            <span className='text-blue-500'> here</span>
+          </a>
+        </div>
+      </ApolloProvider>
     </div>
   )
 }
