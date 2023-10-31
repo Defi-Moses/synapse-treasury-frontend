1ba421e27a3709e106784f04092d4828d993e7a0
manzuni <zuniakis@gmail.com>
2023-10-31 19:09:40 +0000

    Methodology sub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,29 +29,20 @@ export default function Home() {
           </div>
           <h3 className='font-bold font-inter py-4 text-white'>Historical Data</h3>
           <MonthList />
-
-          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-            {/* <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        /> */}
+          <div className='w-1/8 flex justify-center gap-[0.5rem] items-center bg-black mt-4'>
+            <a
+              href='https://github.com/Defi-Moses/synapse-treasury/blob/main/README.md#methodology'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-white'
+            >
+              <sub>
+                * Methodology can be found
+                <span className='text-blue-500'> here</span>
+              </sub>
+            </a>
           </div>
         </main>
-        <div className='w-1/8 flex justify-center gap-[0.5rem] items-center bg-black '>
-          <a
-            href='https://github.com/Defi-Moses/synapse-treasury/blob/main/README.md#methodology'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='text-white'
-          >
-            Methodology can be found
-            <span className='text-blue-500'> here</span>
-          </a>
-        </div>
       </ApolloProvider>
     </div>
   )
