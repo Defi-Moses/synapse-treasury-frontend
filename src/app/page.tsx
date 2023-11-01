d9b50c763b27d8ce7094f8e943ad2f4e36bffe5b
manzuni <zuniakis@gmail.com>
2023-11-01 12:30:42 +0000

    HistoricalTable fix, Part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,9 +28,7 @@ export default function Home() {
             </div>
           </div>
           <h3 className='font-bold font-inter py-4 text-white'>Historical Data</h3>
-          <div className='w-2/3 h-full p-8 border rounded-lg'>
-            <MonthList />
-          </div>
+          <MonthList />
 
           <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
             {/* <Image
